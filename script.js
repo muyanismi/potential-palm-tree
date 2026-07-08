f73faaa45d28a41ca41ef0639600602b262f2ba7
muyanismi <1042947819@qq.com>
2026-07-09 07:12:35 +0800

    Add functionality to save input to localStorage

---
+++ script.js
@@ -0,0 +1,97 @@
+const title =
+document.getElementById("title");
+
+const input =
+document.getElementById("input");
+
+const tip =
+document.getElementById("tip");
+
+
+
+title.onclick=function(){
+
+
+title.style.display="none";
+
+
+input.style.display="block";
+
+
+input.focus();
+
+
+tip.innerHTML=
+"写下此刻的你，按 Enter";
+
+
+}
+
+
+
+input.addEventListener(
+"keydown",
+function(e){
+
+
+if(e.key==="Enter"){
+
+
+let text=input.value.trim();
+
+
+if(text==="") return;
+
+
+
+let record={
+
+text:text,
+
+time:new Date()
+.toLocaleString(),
+
+id:
+"MY-"+Math.floor(Math.random()*999999)
+
+};
+
+
+
+let old=
+JSON.parse(
+localStorage.getItem("myRecord")
+||
+"[]"
+);
+
+
+
+old.push(record);
+
+
+localStorage.setItem(
+"myRecord",
+JSON.stringify(old)
+);
+
+
+
+input.style.display="none";
+
+
+title.innerHTML=
+"我爱你";
+
+
+title.style.display="block";
+
+
+tip.innerHTML=
+"已匿名保存";
+
+
+}
+
+
+});
